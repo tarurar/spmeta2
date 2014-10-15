b56a92b84c755938826692f607a07f73abaae550
tarurar <tarutin_andrew@mail.ru>
2014-10-15 17:54:00 +0400

    behaviour

---
+++ SPMeta2/SPMeta2.CSOM.Behaviours/ListBehaviours.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.SharePoint.Client;
+
+namespace SPMeta2.CSOM.Behaviours
+{
+    public static class ListBehaviours
+    {
+        #region common extensions
+
+        public static List MakeFolderCreationAvailable(this List list, bool available)
+        {
+            list.EnableFolderCreation = available;
+
+            return list;
+        }
+
+        #endregion
+    }
+}
