ba246ef43d8ef4c717c4999eef8190f6dbe50bef
tarurar <tarutin_andrew@mail.ru>
2015-02-27 22:20:35 +0300

    ListView behaviours class

---
+++ SPMeta2/SPMeta2.CSOM.Behaviours/ListViewBehaviours.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.SharePoint.Client;
+
+namespace SPMeta2.CSOM.Behaviours
+{
+    public static class ListViewBehaviours
+    {
+        #region common extensions
+
+        public static View MakeFoldersInvisible(this View view)
+        {
+            view.Scope = ViewScope.Recursive;
+            view.Aggregations = "Off";
+
+            return view;
+        }
+
+        #endregion
+    }
+}
